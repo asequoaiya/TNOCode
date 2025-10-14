fbc7fb06e9d3aa19e20bcb00b3eb6dded3b75817
asequoaiya <kevin.lu05@gmail.com>
2025-10-14 10:35:12 +0200

    Implemented the rest of the uniaxial state code

--- shear_band_thickness.py
+++ shear_band_thickness.py
@@ -3,6 +3,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from icecream import ic
+
+
+# Function definition for cotangent
+def cot(argument):
+    return 1 / np.tan(argument)
+
 
 # Shear band material class definition
 class ShearBandMaterial:
@@ -56,12 +63,9 @@ class ShearBand:
     def a_1(self, theta):
         g = self.material.elastic_shear_modulus()
         g_t = self.material.hardening_shear_modulus()
-        g_s = self.material.softening_shear_modulus()
 
         sigma_y = self.material.yield_strength
         sigma_u = self.material.ultimate_tensile_strength
-
-        l_cs = self.material.intrinsic_length
 
         top_left_bracket = (3 * (np.power(np.cos(theta), 4) + np.power(np.sin(2 * theta), 2))
                             * (sigma_u - sigma_y)
@@ -88,8 +92,6 @@ class ShearBand:
         sigma_y = self.material.yield_strength
         sigma_u = self.material.ultimate_tensile_strength
 
-        l_cs = self.material.intrinsic_length
-
         factor = 2 * sigma_u * g_t * g
         top_left_bracket = 2 * sigma_y * g_s * ((g_t - g) * (np.power(np.sin(2 * theta), 2)
                                                              + np.power(np.cos(theta), 4)))
@@ -109,7 +111,7 @@ class ShearBand:
 
         return value
 
-    def a_3(self, theta):
+    def b_2(self, theta):
         g = self.material.elastic_shear_modulus()
         g_t = self.material.hardening_shear_modulus()
         g_s = self.material.softening_shear_modulus()
@@ -117,7 +119,32 @@ class ShearBand:
         sigma_y = self.material.yield_strength
         sigma_u = self.material.ultimate_tensile_strength
 
-        l_cs = self.material.intrinsic_length
+        factor = g_t * sigma_u
+        top_left_bracket = g * sigma_u * (-64 * g * np.sin(2 * theta)
+                                          + (9 * np.sin(6 * theta)
+                                             + 13 * np.sin(2 * theta)
+                                             - 12 * np.sin(4 * theta) * (g_s - g_t)))
+        top_right_bracket = (-9 * g_s * np.sin(6 * theta)
+                             + (64 * g - 13 * g_s) * np.sin(2 * theta)
+                             + 12 * g_s * np.sin(4 * theta)) * (g - g_t) * sigma_y
+        numerator = factor * (top_left_bracket + top_right_bracket)
+
+        bottom_left_bracket = -32 * g_t * sigma_y + g * (sigma_u - sigma_y)
+        bottom_right_bracket = (3 * (g_t - g) * g_s * sigma_y * (np.power(np.cos(theta), 4))
+                                + 4 * g * g_t * sigma_u
+                                + 3 * g * sigma_u * np.power(np.cos(theta), 4) * (g_s - g_t))
+        denominator = bottom_left_bracket * bottom_right_bracket
+
+        value = numerator / denominator
+
+        return value
+
+    def a_3(self, theta):
+        g = self.material.elastic_shear_modulus()
+        g_t = self.material.hardening_shear_modulus()
+
+        sigma_y = self.material.yield_strength
+        sigma_u = self.material.ultimate_tensile_strength
 
         top_left_bracket = (3 * np.power(np.cos(theta), 4)
                             * (g_t - g)
@@ -137,6 +164,19 @@ class ShearBand:
 
         return value
 
+    def xi(self, theta):
+        sigma_u = self.material.ultimate_tensile_strength
+        epsilon_u = self.material.strain_at_uts
+
+        l_cs = self.material.intrinsic_length
+
+        numerator = 3 * epsilon_u * self.a_1(theta)
+        denominator = sigma_u * l_cs ** 2
+
+        value = (numerator / denominator) ** 0.5
+
+        return value
+
     def eta(self, theta):
         sigma_u = self.material.ultimate_tensile_strength
 
@@ -150,6 +190,40 @@ class ShearBand:
 
         return value
 
+    def a_value(self, theta, thickness):
+        g = self.material.elastic_shear_modulus()
+        g_t = self.material.hardening_shear_modulus()
+
+        sigma_y = self.material.yield_strength
+        sigma_u = self.material.ultimate_tensile_strength
+
+        factor = 1 / np.e ** (-self.xi(theta) * thickness)
+
+        left_bracket = (4 * np.tan(theta)
+                        - 9 * np.sin(2 * theta)
+                        - 12 * np.power(np.sin(theta), 2)
+                        - 6 * sigma_y * np.power(np.cos(theta), 2) * cot(theta) / sigma_u
+                        - 2 * cot(theta)) / (48 * g)
+        right_bracket = (sigma_y * cot(theta) * np.power(np.cos(theta), 2) / sigma_u
+                         - np.power(np.cos(theta), 2) * cot(theta)) / (8 * g_t)
+
+        value = factor * (left_bracket + right_bracket)
+
+        return value
+
+    def c_value(self, theta, thickness):
+        g = self.material.elastic_shear_modulus()
+
+        numerator = (np.sin(2 * theta) / (4 * g)
+                     + self.a_value(theta, thickness) * np.e ** (-self.xi(theta) * thickness)
+                     - self.b_2(theta) / self.a_2(theta))
+        denominator = np.cos(self.eta(theta) * thickness)
+
+        value = numerator / denominator
+
+        return value
+
+
     def band_thickness_uniaxial(self):
         theta = np.deg2rad(np.arange(91))
 
@@ -158,9 +232,10 @@ class ShearBand:
         a_3 = self.a_3(theta)
         eta = self.eta(theta)
 
-
         trig = np.arctan((-a_1 / a_2) ** 0.5 * a_3)
         thickness = (np.pi + trig) / eta
+
+        return thickness
 
     def strain_rate_11(self, theta):
         g = self.material.elastic_shear_modulus()
@@ -173,7 +248,71 @@ class ShearBand:
         return value
 
     def strain_rate_12(self, theta, x2, thickness):
-        return
+        g = self.material.elastic_shear_modulus()
+
+        strain_rate_matrix = np.zeros((len(x2), len(theta)))
+
+        for matrix_row, x2_value in enumerate(x2):
+            for matrix_column, theta_value in enumerate(theta):
+
+                thickness_value = thickness[matrix_column]
+
+                if x2_value < thickness_value:
+                    a = self.a_value(theta_value, thickness_value)
+                    xi = self.xi(theta_value)
+                    ic(a, xi)
+                    strain_rate_matrix[matrix_row][matrix_column] = (np.sin(2 * theta_value) / (4 * g)
+                                                                     + a * np.e ** (-xi * x2_value))
+                else:
+                    b_2 = self.b_2(theta_value)
+                    a_2 = self.a_2(theta_value)
+                    c = self.c_value(theta_value, thickness_value)
+                    eta = self.eta(theta)
+
+                    strain_rate_matrix[matrix_row][matrix_column] = (b_2 / a_2
+                                                                     + c * np.cos(eta * x2_value))
+
+        return strain_rate_matrix
+
+    def strain_rate_22(self, theta, x2, thickness):
+        g = self.material.elastic_shear_modulus()
+        sigma_u = self.material.ultimate_tensile_strength
+        epsilon_u = self.material.strain_at_uts
+
+        strain_rate_21 = self.strain_rate_12(theta, x2, thickness)
+        ratio = (sigma_u - 3 * g * epsilon_u) / epsilon_u
+
+        strain_rate_11 = np.full((np.shape(strain_rate_21)), self.strain_rate_11(theta))
+
+        top_left_bracket = (np.power(np.cos(theta), 2)
+                            + strain_rate_21 * ratio * np.sin(2 * theta) * np.power(np.cos(theta), 2))
+        top_right_bracket = strain_rate_11 * (2 * sigma_u / (3 * epsilon_u)
+                                              - ratio * np.power(np.sin(2 * theta), 2))
+        numerator = top_left_bracket - top_right_bracket
+
+        denominator = (4 * sigma_u / (3 * epsilon_u)
+                       - ratio * np.power(np.cos(2 * theta), 4))
+
+        value = numerator / denominator
+
+        return value
+
+    def overall_effective_strain(self, theta, x2, thickness):
+        term_one = self.strain_rate_12(theta, x2, thickness) * np.sin(2 * theta)
+        term_two = np.full((np.shape(term_one)), self.strain_rate_11(theta)) * np.power(np.sin(theta), 2)
+        term_three = self.strain_rate_22(theta, x2, thickness) * np.power(np.cos(theta), 2)
+
+        value = term_one + term_two + term_three
+
+        return value
+
+    def plot_overall_effective_strain(self):
+        theta = np.arange(19) * 5
+        x2 = np.arange(11) * 10 ** -6
+        thickness = self.band_thickness_uniaxial()
+
+        overall_effective_strain = self.overall_effective_strain(theta, x2, thickness)
+        print(overall_effective_strain)
 
     def thickness(self):
         """
@@ -201,14 +340,14 @@ def get_material_properties(name):
     return properties
 
 
-s235jr_steel = ShearBandMaterial(*get_material_properties("S235JR"))
-s235jr_band = ShearBand('shear', s235jr_steel)
-print(s235jr_band.thickness())
-
-ah36_steel = ShearBandMaterial(*get_material_properties("AH36"))
-ah36_band = ShearBand('shear', ah36_steel)
-print(ah36_band.thickness())
+# s235jr_steel = ShearBandMaterial(*get_material_properties("S235JR"))
+# s235jr_band = ShearBand('shear', s235jr_steel)
+# print(s235jr_band.thickness())
+#
+# ah36_steel = ShearBandMaterial(*get_material_properties("AH36"))
+# ah36_band = ShearBand('shear', ah36_steel)
+# print(ah36_band.thickness())
 
 s355_steel = ShearBandMaterial(*get_material_properties("S355"))
 s355_band = ShearBand('uniaxial', s355_steel)
-print(s355_band.thickness())
+s355_band.plot_overall_effective_strain()
